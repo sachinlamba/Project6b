6bdab2889dc65d6ba46ec50893c1036d82ff2133
sachin lamba <sachin.lamba@moldedbits.com>
2016-06-24 11:54:32 +0530

    Initial Commit

---
+++ Project6b/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project6b
+//
+//  Created by Sachin Lamba on 24/06/16.
+//  Copyright © 2016 LambaG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
