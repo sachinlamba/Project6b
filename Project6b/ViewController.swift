58e12864d03a9455b07dd6d03bd5685d3275bbc4
sachin lamba <sachin.lamba@moldedbits.com>
2016-06-24 13:07:37 +0530

    Label height can change according to orientation.

--- Project6b/ViewController.swift
+++ Project6b/ViewController.swift
@@ -51,7 +51,7 @@ class ViewController: UIViewController {
             view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-[\(label)]-|", options: [], metrics: nil, views: viewsDictionary))
         }
         
-        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-[label1]-[label2]-[label3]-[label4]-[label5]", options: [], metrics: nil, views: viewsDictionary))
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: ["labelHeight": 80], views: viewsDictionary))
     }
     
     override func prefersStatusBarHidden() -> Bool {
