bd566b4b67cac661b1ed754aa8c3318e7dcb99c3
sachin lamba <sachin.lamba@moldedbits.com>
2016-06-24 12:55:08 +0530

    Labels Layout is changed by Visual format language(VFL) to separate them.

--- Project6b/ViewController.swift
+++ Project6b/ViewController.swift
@@ -45,6 +45,13 @@ class ViewController: UIViewController {
         view.addSubview(label4)
         view.addSubview(label5)
         
+        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5 ]
+        
+        for label in viewsDictionary.keys {
+            view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-[\(label)]-|", options: [], metrics: nil, views: viewsDictionary))
+        }
+        
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-[label1]-[label2]-[label3]-[label4]-[label5]", options: [], metrics: nil, views: viewsDictionary))
     }
     
     override func prefersStatusBarHidden() -> Bool {
